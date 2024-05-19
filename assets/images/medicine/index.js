fad599ffee74b9aec08934f41cf5361ec3d08e18
Nekro06 <matt06.antonio@gmail.com>
2024-05-19 12:04:27 +0800

    added product images and labels

--- assets/images/medicine/index.js
+++ assets/images/medicine/index.js
@@ -1,3 +1,17 @@
 export default {
 	"paracetamol": require("./paracetamol.png"),
+	"omeprazole-20mg": require("./omeprazole-20mg.png"),
+	"omeprazole-40mg": require("./omeprazole-40mg.png"),
+	"trimetazidine-30mg": require("./trimetazidine-30mg.png"),
+	"amlodipine-besilate-5mg": require("./amlodipine-besilate-5mg.png"),
+	"amlodipine-besilate-10mg": require("./amlodipine-besilate-10mg.png"),
+	"losartan-potassium": require("./losartan-potassium.png"),
+	"rosuvastatin-10mg": require("./rosuvastatin-10mg.png"),
+	"rosuvastatin-20mg": require("./rosuvastatin-20mg.png"),
+	"metformin-hci": require("./metformin-hci.png"),
+	"telmisartan-40mg": require("./telmisartan-40mg.png"),
+	"telmisartan+h-40mg": require("./telmisartan+h-40mg.png"),
+	"carvedilol-25mg": require("./carvedilol-25mg.png"),
+	"clodipogrel-75mg": require("./clodipogrel-75mg.png"),
+	
 }
